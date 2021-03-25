959c20ac9447d961d30048a460333119c1d167f2
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2021-03-25 01:25:06 -0300

    linhas de código com kotlin

---
+++ src/main/kotlin/me/beafe/TesteColecoesMutaveis.kt
@@ -0,0 +1,30 @@
+package me.beafe
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0,"PJ")
+    val maria = Funcionario("Maria", 4000.0,"CLT")
+
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach{ println(it)}
+
+    println("============== LIST ====================")
+    funcionarios.add(pedro)
+    funcionarios.forEach{ println(it)}
+
+    println("==================================")
+    funcionarios.remove(joao)
+    funcionarios.forEach{ println(it)}
+
+    println("============== SET ====================")
+    val funcionarioSet = mutableListOf(joao)
+    // funcionarios.forEach{ println(it)}
+
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach{ println(it)}
+
+    println("==============")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach{ println(it)}
+}
